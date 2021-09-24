9ea49b125c594cb909939cd7ca1fe60740ac181c
Henrique <riquessan@gmail.com>
2021-09-24 12:31:24 -0300

    Ignore Dist

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-interface IProps {
-    children?: React.ReactNode;
-    callback: () => any;
-}
-declare const Sentineler: ({ children, callback }: IProps) => JSX.Element;
-export default Sentineler;
